72398393f6a69df095fc418b4b4f864312b57ff1
Ruikai Liu <lrk700@gmail.com>
2017-08-11 09:16:36 +0800

    YAHFA check if the hook and origin are static methods

--- VirtualApp/YAHFA/src/main/java/lab/galaxy/yahfa/HookMain.java
+++ VirtualApp/YAHFA/src/main/java/lab/galaxy/yahfa/HookMain.java
@@ -59,9 +59,9 @@ public class HookMain {
             Method hook = null;
             Method backup = null;
             for (Method method : hookItem.getDeclaredMethods()) {
-                if (method.getName().equals("hook")) {
+                if (method.getName().equals("hook") && Modifier.isStatic(method.getModifiers())) {
                     hook = method;
-                } else if (method.getName().equals("origin")) {
+                } else if (method.getName().equals("origin")  && Modifier.isStatic(method.getModifiers())) {
                     backup = method;
                 }
             }
@@ -82,7 +82,7 @@ public class HookMain {
         try {
             int hookParamCount = hook.getParameterTypes().length;
             int targetParamCount = getParamCountFromSignature(methodSig);
-//            Log.d(TAG, "target method param count is "+targetParamCount);
+            // Log.d(TAG, "target method param count is "+targetParamCount);
             boolean isStatic = (hookParamCount == targetParamCount);
             // virtual method has 'thiz' object as the first parameter
             findAndBackupAndHook(targetClass, methodName, methodSig, isStatic, hook, backup);
